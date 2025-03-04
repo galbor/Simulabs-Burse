71aef2623e12646618e93ef310e2cecec3c8ee4b
galborovoi <gal.borovoi@huji.mail.ac.il>
2025-03-04 22:29:29 +0200

    Moved RegularPriceChanger because it's location was wrong

--- Simulabs Burse Console/PriceChanger/RegularPriceChanger.cs
+++ Simulabs Burse Console/PriceChanger/RegularPriceChanger.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Threading;
 using Microsoft.VisualBasic;
-using Simulabs_Burse_Console.PriceChanger;
 using Simulabs_Burse_Console.Stock_Market;
 
-namespace Simulabs_Burse_Console.POD;
+namespace Simulabs_Burse_Console.PriceChanger;
 
-public class RegularPriceChanger (ICollection<IStockMarket.CompanyAndPrice> collection, 
-    INewPriceCalculator priceCalculator,int sleepTime = 20000) : IPriceChanger
+public class RegularPriceChanger(ICollection<IStockMarket.CompanyAndPrice> collection,
+    INewPriceCalculator priceCalculator, int sleepTime = 20000) : IPriceChanger
 {
     public ICollection<IStockMarket.CompanyAndPrice> Collection { get; } = collection;
     public int SleepTime { get; set; } = sleepTime;
